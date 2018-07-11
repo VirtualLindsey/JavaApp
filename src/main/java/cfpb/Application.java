d5acb4dfb820ead4effe9c863a6ac0522f8b8ee0
VirtualLindsey <tapircode@gmail.com>
2018-07-10 23:05:49 -0700

    adding index call

--- src/main/java/cfpb/Application.java
+++ src/main/java/cfpb/Application.java
@@ -2,12 +2,32 @@ package cfpb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String index(){ return "index";}
+
+    @GetMapping("/single")
+    public String single(@RequestParam(name="name", required=false, defaultValue="single") String name, Model model) {
+        model.addAttribute("name", name);
+        return "single";
+    }
+
+    @GetMapping("/compare")
+    public String compare(@RequestParam(name="name", required=false, defaultValue="compare") String name, Model model) {
+        model.addAttribute("name", name);
+        return "compare";
     }
 
 }
