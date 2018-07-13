72fe24da309d3f23ea5ecc59d9de9e8063db874b
VirtualLindsey <tapircode@gmail.com>
2018-07-12 22:58:47 -0700

    updating stlying

--- src/main/resources/static/events.js
+++ src/main/resources/static/events.js
@@ -156,8 +156,8 @@ $(document).ready(function(){
                     // Set chart options
                     var title = count + ' complaints for: ' + $("#typeAheadID").val();
                     var options = {'title': title,
-                        'width':200,
-                        'height':200};
+                        'width':300,
+                        'height':300};
 
                     var chart_id = "";
                     var id = 0;
