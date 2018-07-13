4eb9a4cb31711c496eae40f09486a5231ff71246
VirtualLindsey <tapircode@gmail.com>
2018-07-12 22:11:44 -0700

    changing callback function

--- src/main/resources/static/events.js
+++ src/main/resources/static/events.js
@@ -64,7 +64,9 @@ $(document).ready(function(){
                 google.charts.load('current', {'packages':['corechart']});
 
                 // Set a callback to run when the Google Visualization API is loaded.
-                google.charts.setOnLoadCallback(drawChart(data.length));
+                google.charts.setOnLoadCallback(function(){
+                    drawChart(data.length);
+                });
 
                 // Callback that creates and populates a data table,
                 // instantiates the pie chart, passes in the data and
